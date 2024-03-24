8c5b8cfe09f58c91ca38e818538905f926b0a4d8
abduvalee95 <abduvalee95@gmail.com>
2024-03-24 09:24:16 +0900

    feat: MUI standart integrations

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import '../css/app.css';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
 
 function App() {
-  return <div>Home  </div>
+  return ( 
+  <Container maxWidth="sm">
+    <Stack flexDirection={"column"}>
+      <Box sx={{my:4}}>
+        <Typography variant='h4' component={'h4'}  >
+          Creaty React app  on Typescript & Redux
+        </Typography>
+      <Button variant='contained'> Contained Button </Button>
+      </Box>
+      <Button variant='outlined'> Contained Button </Button>
+      <Button variant='outlined' color='error'> Contained Button </Button>
+    </Stack>
+  </Container>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import './css/index.css';
 
 
@@ -11,7 +14,10 @@ import './css/index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}> 
+      <CssBaseline />
+       <App /> 
+       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')!
