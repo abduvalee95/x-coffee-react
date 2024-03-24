f7133d30e4d8fbca50510fb00e48e85edaf838df
abduvalee95 <abduvalee95@gmail.com>
2024-03-24 20:53:14 +0900

    feat: create routing system

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import '../css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import { RippleBadge } from './MaterialTheme/styled';
-import { orange } from '@mui/material/colors';
+import { Link, Route, Router, Switch } from 'react-router-dom';
+import { Users } from './MaterialTheme/screens/Users';
+import { About } from './MaterialTheme/screens/About';
 
+/* 
 function App() {
   return ( 
   <Container sx={{background:'orange'}}>
     <Stack flexDirection={"column"}>
-      <Box sx={{my:4}}>
+      <Box sx={{my:8}}>
         <Typography variant='h4' component={'h4'}  >
           Creaty React app  on Typescript & Redux
         </Typography>
@@ -21,6 +24,48 @@ function App() {
     </Stack>
   </Container>
   );
+} 
+*/
+
+export function App() {
+  return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+  );
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+
+
 
 export default App;
