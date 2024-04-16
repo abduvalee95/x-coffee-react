5d8b0e934a90ad57bd0d3bc1729fc0028923396c
abduvalee95 <abduvalee95@gmail.com>
2024-04-16 20:19:16 +0900

    feat: practice useState & useEffect hooks

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,87 +1,134 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
+import React, { useState, useEffect } from "react";
+import { log } from "console";
 
-export default function HomeNavbar(){
-    const authMember = null;
+export default function HomeNavbar() {
+  const authMember = null;
 
-    return (
-    <div className="home-navbar"> 
-    <Container className="navbar-container">
+  const [count, setCount] = useState<number>(0),
+    [value, setValue] = useState<boolean>(true); // button bosilganda qiymati ozgaryabti True bolsa false false bolsa true ga ozgaryabti
+
+  useEffect(() => {
+    console.log("commponentDidmount"); // data Fetch qilamiz
+      setCount(count + 1);
+      
+      // return orqalik component willUnmount ni iwlatishimiz mm
+      return () => {
+          console.log("componentWillUnMount");
+          
+      }
+  }, [value]); // bunda calback function hamda arraydependency bor "nimani qymati ozgarganda ishga tushsin "
+  //valueni qiymati ozgarganligi sababli useEffect yana ishga tuwub beradi
+  // doim birmarta ishga tushadi  arraydependencyga malum bir valueni qymatni kiritsak  aynan valueni qiymati opzgargan vaqtda ishga tuwiw  mexanizmni qurib beradi;
+  // Valueni qiymati ozgarganda componentimiz yana ishga tushadi
+  const buttonHandler = () => {
+    setValue(!value);
+  };
+
+  /* Hendlers  */
+  // count nomli qiymatni hozil qilib ber boshlangich qiymati (0) bolsin
+  //   const [count, setCount] = useState<number>(0);
+  // setcount dan foydalanamiz button bosilganda qiymatmiz ozgarish kk
+
+  // 2chi variant
+
+  /*  
+ const buttonHandler = () => {
+    setCount(count + 2);
+}; 
+*/
+
+  return (
+    <div className="home-navbar">
+      <Container className="navbar-container">
         <Stack className="menu">
-            <Box>
-                <NavLink to="/">
-                    <img className="brand-logo"
-                    src="/icons/burak.svg"
-                    />
-                </NavLink>
+          <Box>
+            <NavLink to="/">
+              <img className="brand-logo" src="/icons/burak.svg" />
+            </NavLink>
+          </Box>
+
+          <Stack
+            className="links" // vertical
+          >
+            <Box className={"hover-line"}>
+              <NavLink to="/" activeClassName={"underline"}>
+                Home
+              </NavLink>
             </Box>
 
-            <Stack className="links" // vertical
-            >
-                <Box className={"hover-line"} >
-                    <NavLink to="/" activeClassName={"underline"}> 
-                    Home 
-                    </NavLink>
-                </Box>
-                
-                <Box className={"hover-line"} >
-                <NavLink to="/products" activeClassName={"underline"}> 
-                Products 
-                </NavLink>
-                </Box>
-                
-                {authMember ? (
-                <Box className={"hover-line"} >
-                <NavLink to="/orders" activeClassName={"underline"}> 
-                Orders 
-                </NavLink>
-                </Box>
-               ) : null}
+            <Box className={"hover-line"}>
+              <NavLink to="/products" activeClassName={"underline"}>
+                Products
+              </NavLink>
+            </Box>
 
-                {authMember ? (
-                <Box className={"hover-line"} >  
-                <NavLink to="/member-page" activeClassName={"underline"}> 
-                MyPage 
+            {authMember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/orders" activeClassName={"underline"}>
+                  Orders
                 </NavLink>
-                </Box>
-               ) : null}
-                
-                <Box className={"hover-line"} >
-                <NavLink to="/help" activeClassName={"underline"} >
-                     Help 
-                     </NavLink>
-                </Box>
-            <Basket/>
+              </Box>
+            ) : null}
+
+            {authMember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/member-page" activeClassName={"underline"}>
+                  MyPage
+                </NavLink>
+              </Box>
+            ) : null}
+
+            <Box className={"hover-line"}>
+              <NavLink to="/help" activeClassName={"underline"}>
+                Help
+              </NavLink>
+            </Box>
+            <Basket />
             {!authMember ? (
-           <Box>
-                <Button 
-                variant="contained" 
-                className="login-button"> Login </Button>
-            </Box>
-           ) : (
-                <img className="user-avatar" src={"/icons/default-user.svg"} aria-haspopup={"true"}/>
+              <Box>
+                <Button variant="contained" className="login-button">
+                  {" "}
+                  Login{" "}
+                </Button>
+              </Box>
+            ) : (
+              <img
+                className="user-avatar"
+                src={"/icons/default-user.svg"}
+                aria-haspopup={"true"}
+              />
             )}
-            </Stack>
-            </Stack>
-        <Stack className={"header-frame"}> 
-            <Stack className={"detail"}> 
-                <Box className={"head-main-txt"}>World's Most Delicious Cousine</Box>
-                <Box className={"wel-txt"}>The Choice, not just a choice</Box>
-                <Box className={"service-txt"}>24 hours service</Box>
-                <Box className={"signup"}>
-                    {!authMember ? (
-                    <Button variant="contained" className={"signup-button"}> 
-                    SIGN UP
-                    </Button>
-                    ) : null}    
-                </Box>
-            </Stack>
-            <Box className={"logo-frame"}> 
-                <div className={"logo-img"}></div>
-            </Box>
+          </Stack>
         </Stack>
-    </Container>
+        <Stack className={"header-frame"}>
+          <Stack className={"detail"}>
+            <Box className={"head-main-txt"}>
+              World's Most Delicious Cousine
+            </Box>
+            <Box className={"wel-txt"}>The Choice, not just a choice</Box>
+            <Box className={"service-txt"}>{count} hours service</Box>
+            <Box className={"signup"}>
+              {!authMember ? (
+                <Button
+                  variant="contained"
+                  className={"signup-button"}
+                  /*  onClick={() => setCount( count +1 )} */ onClick={
+                    buttonHandler
+                  }
+                >
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Box className={"logo-frame"}>
+            <div className={"logo-img"}></div>
+          </Box>
+        </Stack>
+      </Container>
     </div>
- );
+  );
 }
