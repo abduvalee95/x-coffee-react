c15630ecc5bfb99d267b0df2d6f5beb37af69bad
abduvalee95 <abduvalee95@gmail.com>
2024-04-19 07:46:52 +0900

    feat: develop slice & selector usage

--- src/app/screens/homePage/selector.ts
+++ src/app/screens/homePage/selector.ts
@@ -7,7 +7,7 @@ const selectHomePage = (state: AppRootState) => state.homePage;
 // retrieve qilishda foydalanamiz
 export const retrievePopularDishes = createSelector(
   selectHomePage,
-  (HomePage) => HomePage.popularDishes
+  (HomePage) => HomePage.popularDishes //selector esa olgan detani olib beryabti 
 );
 
 export const retrieveNewDishes = createSelector(
