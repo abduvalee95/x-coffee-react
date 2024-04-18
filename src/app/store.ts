7af26f7a1c5ec5adbcd1386c019d8362842f1ad8
abduvalee95 <abduvalee95@gmail.com>
2024-04-18 21:29:17 +0900

    feat: middleware integration redux logger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,15 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import HomePageReduser from "./screens/homePage/slice";
+import reduxLogger from "redux-logger"
+
 
 export const store = configureStore({
+
+  middleware: (getDefaultMiddleware) =>
+    //@ts-ignore
+    getDefaultMiddleware().concat(reduxLogger),
+  // qanday ishlar amalga owirilyatganini log qilib beradi 
+  
   reducer: {
     // homepageni ichidagi Slice bn Reduser Storega boglandi
     homePage: HomePageReduser,
